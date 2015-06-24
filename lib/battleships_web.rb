207a5035fa986ed1a72dbc70717565f7f9d897ec
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-06-24 15:36:10 +0100

    can add all ships

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -14,16 +14,31 @@ class BattleshipsWeb < Sinatra::Base
   get '/newgame' do
     $game = Game.new Player, Board
     @submarine_coordinate = params[:submarine_coordinate]
-    @submarine_direction = params[:submarine_direction]
-    @destroyer_coordinate = params[:destroyer_coordinate]
-    @destroyer_direction = params[:destroyer_direction]
-    @cruiser_coordinate = params[:cruiser_coordinate]
-    @cruiser_direction = params[:cruiser_direction]
-    @battleship_coordinate = params[:battleship_coordinate]
-    @battleship_direction = params[:battleship_direction]
-    @aircraft_carrier_coordinate = params[:aircraft_carrier_coordinate]
-    @aircraft_carrier_direction = params[:aircraft_carrier_direction]
+    @submarine_direction  = params[:submarine_direction]
     erb :newgame
+  end
+
+  get '/destroyer' do
+    @destroyer_coordinate = params[:destroyer_coordinate]
+    @destroyer_direction  = params[:destroyer_direction]
+    erb :destroyer
+  end
+
+  get '/cruiser' do
+    @cruiser_coordinate = params[:cruiser_coordinate]
+    @cruiser_direction  = params[:cruiser_direction]
+    erb :cruiser
+  end
+  get '/battleship' do
+    @battleship_coordinate = params[:battleship_coordinate]
+    @battleship_direction  = params[:battleship_direction]
+    erb :battleship
+  end
+
+  get '/aircraft_carrier' do
+    @aircraft_carrier_coordinate = params[:aircraft_carrier_coordinate]
+    @aircraft_carrier_direction  = params[:aircraft_carrier_direction]
+    erb :aircraft_carrier
   end
 
   set :views, proc { File.join(root, '..', 'views') }
